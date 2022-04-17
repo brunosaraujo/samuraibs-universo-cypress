78e46af5982dc3b753213840f4fcfbbb21f08e95
Bruno Araujo <brunosaraujo>
2022-04-17 13:52:54 -0300

    Inclusão do caso de testes de troca de senha

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -2,7 +2,7 @@ import { el } from './elements'
 
 class Toast {
   shouldHaveText(expectTeste) {
-    cy.get(el.toast)
+    cy.get(el.toast, {timeout: 10000})
       .should('be.visible')
       .find('p')
       .should('have.text', expectTeste)
